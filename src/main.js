57150fd0aec4d65a438ed319ecddb1b8d3f142e2
Corvette111 <rhj980119@163.com>
2021-04-27 23:08:06 +0800

    导航栏

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import './assets/css/bootstrap.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')
